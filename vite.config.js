4ccd771ee14ff9736bac2be858e778068565f674
M-Hidayatullah <hidayatullahblk@gmail.com>
2023-12-25 14:18:01 +0800

    edit2

--- vite.config.js
+++ vite.config.js
@@ -7,36 +7,34 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/<USERNAME>, set base to '/'.
+    // If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/<USERNAME>, set base to '/'.
 
-  // If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/, for example your repository is at https://github.com/<USERNAME>/<REPO_NAME>, then set base to '/<REPO_NAME>/'.
+    // If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/, for example your repository is at https://github.com/<USERNAME>/<REPO_NAME>, then set base to '/<REPO_NAME>/'.
 
-  base: '/gitprofile/',
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        navigateFallback: undefined,
-      },
-      includeAssets: ['logo.png'],
-      manifest: {
-        name: 'Portfolio',
-        short_name: 'Portfolio',
-        description: 'Personal Portfolio',
-        icons: [
-          {
-            src: 'logo.png',
-            sizes: '64x64 32x32 24x24 16x16 192x192 512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
-  css: {
-    postcss: {
-      plugins: [tailwind(tailwindConfig), autoprefixer],
+    base: '/',
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                navigateFallback: undefined,
+            },
+            includeAssets: ['logo.png'],
+            manifest: {
+                name: 'Portfolio',
+                short_name: 'Portfolio',
+                description: 'Personal Portfolio',
+                icons: [{
+                    src: 'logo.png',
+                    sizes: '64x64 32x32 24x24 16x16 192x192 512x512',
+                    type: 'image/png',
+                }, ],
+            },
+        }),
+    ],
+    css: {
+        postcss: {
+            plugins: [tailwind(tailwindConfig), autoprefixer],
+        },
     },
-  },
 });
